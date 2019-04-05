f9849e568429663ac4165da7e5d72ba09831eda2
Chris <chris@chrisbull.com>
2019-04-05 14:37:11 -0700

    add missing deps

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import './App.css'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
 
-const middlewareLink = new ApolloLink((operation, forward) => {
+const middlewareLink = new ApolloLink((operation, forward: any) => {
   // get the authentication token from local storage if it exists
   const tokenValue = localStorage.getItem(AUTH_TOKEN)
   // return the headers to the context so httpLink can read them
